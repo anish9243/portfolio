02e3c01c058c5e5dad5cfbceff700564410ba68f
Anish <administrator@anishs-mbp.home>
2024-08-15 20:39:41 -0400

    changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -17,7 +17,6 @@ import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const contactInfo = [
-const contactInfo = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
@@ -33,7 +32,6 @@ const contactInfo = [
     title: "Address",
     description: "Toronto, ON, Canada",
   },
-];
 ];
 
 const Contact = () => {
@@ -197,8 +195,6 @@ const Contact = () => {
               </div>
               {/* Service Selection */}
               <Select name="service" onValueChange={handleSelectChange} required>
-              {/* Service Selection */}
-              <Select name="service" onValueChange={handleSelectChange} required>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
@@ -220,7 +216,6 @@ const Contact = () => {
                 value={formData.message}
                 onChange={handleChange}
                 required
-                required
               />
               {/* Submit Button */}
               <Button size="md" className="max-w-40" type="submit">
@@ -241,7 +236,6 @@ const Contact = () => {
           {/* Contact Information */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-10">
-              {contactInfo.map((item, index) => (
               {contactInfo.map((item, index) => (
                 <li key={index} className="flex items-center gap-6">
                   <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
